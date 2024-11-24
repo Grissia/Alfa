f4109109c66539e33428f277703715c7e6ea410c
Grissia <wasd.lhe@gmail.com>
2024-11-25 07:32:39 +0800

    "feat: adding temp"

--- Alfa/ContentView.swift
+++ Alfa/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue, .white], startPoint: .topTrailing, endPoint: .bottomLeading)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Taipei, Taiwan")
+                    .font(.system(size:32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack {
+                    Image(systemName: "cloud")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("18C").font(.system(size: 60, weight: .medium))
+                    
+                        
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
